0399bd0b58033ae6dbb02aa6179d4487fb76400a
LD <LDinho@users.noreply.github.com>
2019-12-14 00:09:57 -0500

    add soln to iterative sorting

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,26 +1,33 @@
-# TO-DO: Complete the selection_sort() function below 
-def selection_sort( arr ):
+# TO-DO: Complete the selection_sort() function below
+def selection_sort(arr):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
         # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
-
-
-
+        # (hint, can do in 3 loc)
+        for j in range(cur_index, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
         # TO-DO: swap
-
-
-
-
+        temp_smallest_value = arr[smallest_index]
+        arr[smallest_index] = arr[cur_index]
+        arr[cur_index] = temp_smallest_value
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
-def bubble_sort( arr ):
-
+def bubble_sort(arr):
+    is_swapped = True
+    while is_swapped:
+        is_swapped = False
+        for i in range(0, len(arr)-1):
+            if arr[i] > arr[i+1]:
+                # swap occurs here if line 25 is true
+                placeholder = arr[i]
+                arr[i] = arr[i+1]
+                arr[i+1] = placeholder
+                is_swapped = True
     return arr
 
 
